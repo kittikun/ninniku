b0fde9387da51771ef7292d7c745f55b0fc79088
kittikun <me@kittikun.jp>
2020-06-06 19:08:00 +0900

    Forgot to add shaders

---
+++ unit_test/src/shaders/dx11/sameResource.hlsl
@@ -0,0 +1,41 @@
+// Copyright(c) 2018-2020 Kitti Vongsay
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include"../cbuffers.h"
+#include "../dispatch.h"
+
+Texture2DArray<unorm float4> srcTex;
+RWTexture2DArray<unorm float4> dstTex;
+
+[numthreads(SAME_RESOURCE_X, SAME_RESOURCE_Y, SAME_RESOURCE_Z)]
+void main(uint3 DTI : SV_DispatchThreadID)
+{
+    if (targetMip == 0) {
+        dstTex[DTI] = float4(0, 0, 0, 1);
+	} else {
+		uint w, h, e;
+
+		dstTex.GetDimensions(w, h, e);
+
+		if (all(DTI.xy < uint2(w, h))) {
+			dstTex[DTI] = float4(srcTex[DTI].rgb + 0.1f, 1);
+		}
+    }
+}
