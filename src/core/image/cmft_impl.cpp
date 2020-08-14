6d034931f4a96924145133ac675e48466f64cb3b
kittikun <me@kittikun.jp>
2020-08-14 14:21:52 +0900

    Update stb to latest

--- src/core/image/cmft_impl.cpp
+++ src/core/image/cmft_impl.cpp
@@ -30,7 +30,7 @@
 #include "../../utils/misc.h"
 
 #pragma warning(push)
-#pragma warning(disable:6001 6011 6385 6386 6387)
+#pragma warning(disable:4018 4389 6001 6011 6385 6386 6387)
 #define TINYEXR_IMPLEMENTATION
 #include <tinyexr/tinyexr.h>
 #pragma warning(pop)
