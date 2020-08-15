16c7c72a6a5dfcc5ff84ca80cdc9f012c0db1c95
kittikun <me@kittikun.jp>
2020-08-16 02:28:37 +0900

    soem tests were disabled

--- unit_test/src/tests/image.cpp
+++ unit_test/src/tests/image.cpp
@@ -791,114 +791,114 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc5_16bit, T, FixturesAll, T)
 #endif
 }
 
-//BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc6h, T, FixturesAll, T)
-//{
-//    // Disable HW GPU support when running on CI
-//    if (T::isNull)
-//        return;
-//
-//    // skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
-//#ifdef _DEBUG
-//    if (IsAppVeyor())
-//        return;
-//#endif
-//
-//    auto image = std::make_unique<ninniku::cmftImage>();
-//
-//    BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
-//
-//    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
-//    auto& dx = ninniku::GetRenderer();
-//    auto srcTex = dx->CreateTexture(srcParam);
-//    auto res = std::make_unique<ninniku::ddsImage>();
-//
-//    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, srcTex.get()));
-//
-//    std::string filename = "dds_saveImage_bc6h.dds";
-//
-//    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC6H_UF16));
-//    BOOST_REQUIRE(std::filesystem::exists(filename));
-//
-//    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
-//    switch (dx->GetType()) {
-//        case ninniku::ERenderer::RENDERER_DX12:
-//        case ninniku::ERenderer::RENDERER_WARP_DX12:
-//            CheckFileCRC(filename, 4073542973);
-//            break;
-//
-//        case ninniku::ERenderer::RENDERER_DX11:
-//            CheckFileCRC(filename, 842772517);
-//            break;
-//
-//        case ninniku::ERenderer::RENDERER_WARP_DX11:
-//            CheckFileCRC(filename, 842772517);
-//            break;
-//
-//        default:
-//            throw std::exception("case should not happen");
-//            break;
-//    }
-//}
-//
-//BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc7, T, FixturesAll, T)
-//{
-//    // Disable HW GPU support when running on CI
-//    if (T::isNull)
-//        return;
-//
-//    // skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
-//#ifdef _DEBUG
-//    if (IsAppVeyor())
-//        return;
-//#endif
-//
-//    auto& dx = ninniku::GetRenderer();
-//
-//    // There is something wrong with WARP but it's working fine for DX12 HW so disable it
-//    if (dx->GetType() == ninniku::ERenderer::RENDERER_WARP_DX12) {
-//        return;
-//    }
-//
-//    auto image = std::make_unique<ninniku::genericImage>();
-//
-//    BOOST_REQUIRE(image->Load("data/banner.png"));
-//
-//    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
-//    auto srcTex = dx->CreateTexture(srcParam);
-//    auto needFix = image->IsRequiringFix();
-//    auto resized = ResizeImage(dx, srcTex, needFix, T::shaderRoot);
-//    auto res = std::make_unique<ninniku::ddsImage>();
-//
-//    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, resized.get()));
-//
-//    std::string_view filename = "dds_saveImage_bc7.dds";
-//
-//    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC7_UNORM));
-//    BOOST_REQUIRE(std::filesystem::exists(filename));
-//
-//    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
-//    switch (dx->GetType()) {
-//        case ninniku::ERenderer::RENDERER_DX12:
-//            CheckFileCRC(filename, 2657823934);
-//            break;
-//
-//        case ninniku::ERenderer::RENDERER_DX11:
-//            CheckFileCRC(filename, 3153192394);
-//            break;
-//
-//        case ninniku::ERenderer::RENDERER_WARP_DX11:
-//            CheckFileCRC(filename, 2046772967);
-//            break;
-//
-//        case ninniku::ERenderer::RENDERER_WARP_DX12:
-//            throw new std::exception("Invalid test, shouldn't happen");
-//            break;
-//
-//        default:
-//            throw std::exception("case should not happen");
-//            break;
-//    }
-//}
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc6h, T, FixturesAll, T)
+{
+    // Disable HW GPU support when running on CI
+    if (T::isNull)
+        return;
+
+    // skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
+#ifdef _DEBUG
+    if (IsAppVeyor())
+        return;
+#endif
+
+    auto image = std::make_unique<ninniku::cmftImage>();
+
+    BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
+
+    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
+    auto& dx = ninniku::GetRenderer();
+    auto srcTex = dx->CreateTexture(srcParam);
+    auto res = std::make_unique<ninniku::ddsImage>();
+
+    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, srcTex.get()));
+
+    std::string filename = "dds_saveImage_bc6h.dds";
+
+    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC6H_UF16));
+    BOOST_REQUIRE(std::filesystem::exists(filename));
+
+    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
+    switch (dx->GetType()) {
+        case ninniku::ERenderer::RENDERER_DX12:
+        case ninniku::ERenderer::RENDERER_WARP_DX12:
+            CheckFileCRC(filename, 4073542973);
+            break;
+
+        case ninniku::ERenderer::RENDERER_DX11:
+            CheckFileCRC(filename, 842772517);
+            break;
+
+        case ninniku::ERenderer::RENDERER_WARP_DX11:
+            CheckFileCRC(filename, 842772517);
+            break;
+
+        default:
+            throw std::exception("case should not happen");
+            break;
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc7, T, FixturesAll, T)
+{
+    // Disable HW GPU support when running on CI
+    if (T::isNull)
+        return;
+
+    // skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
+#ifdef _DEBUG
+    if (IsAppVeyor())
+        return;
+#endif
+
+    auto& dx = ninniku::GetRenderer();
+
+    // There is something wrong with WARP but it's working fine for DX12 HW so disable it
+    if (dx->GetType() == ninniku::ERenderer::RENDERER_WARP_DX12) {
+        return;
+    }
+
+    auto image = std::make_unique<ninniku::genericImage>();
+
+    BOOST_REQUIRE(image->Load("data/banner.png"));
+
+    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
+    auto srcTex = dx->CreateTexture(srcParam);
+    auto needFix = image->IsRequiringFix();
+    auto resized = ResizeImage(dx, srcTex, needFix, T::shaderRoot);
+    auto res = std::make_unique<ninniku::ddsImage>();
+
+    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, resized.get()));
+
+    std::string_view filename = "dds_saveImage_bc7.dds";
+
+    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC7_UNORM));
+    BOOST_REQUIRE(std::filesystem::exists(filename));
+
+    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
+    switch (dx->GetType()) {
+        case ninniku::ERenderer::RENDERER_DX12:
+            CheckFileCRC(filename, 2657823934);
+            break;
+
+        case ninniku::ERenderer::RENDERER_DX11:
+            CheckFileCRC(filename, 3153192394);
+            break;
+
+        case ninniku::ERenderer::RENDERER_WARP_DX11:
+            CheckFileCRC(filename, 2046772967);
+            break;
+
+        case ninniku::ERenderer::RENDERER_WARP_DX12:
+            throw new std::exception("Invalid test, shouldn't happen");
+            break;
+
+        default:
+            throw std::exception("case should not happen");
+            break;
+    }
+}
 
 BOOST_FIXTURE_TEST_CASE(generic_load, SetupFixtureNull)
 {
