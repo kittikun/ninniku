be87306b6462be20ddd8a20f34e10205704666ba
kittikun <me@kittikun.jp>
2020-08-21 02:09:32 +0900

    fix build error

--- unit_test/src/tests/long.cpp
+++ unit_test/src/tests/long.cpp
@@ -42,119 +42,119 @@ BOOST_AUTO_TEST_SUITE(Long)
 
 BOOST_FIXTURE_TEST_CASE(cmft_load, SetupFixtureNull)
 {
-	auto image = std::make_unique<ninniku::cmftImage>();
+    auto image = std::make_unique<ninniku::cmftImage>();
 
-	BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
+    BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
 
-	auto& data = image->GetData();
+    auto& data = image->GetData();
 
-	CheckCRC(std::get<0>(data), std::get<1>(data), 3196376208);
+    CheckCRC(std::get<0>(data), std::get<1>(data), 3196376208);
 
-	BOOST_REQUIRE(image->Load("data/park02.exr"));
-	auto& data2 = image->GetData();
-	CheckCRC(std::get<0>(data2), std::get<1>(data2), 2283193732);
+    BOOST_REQUIRE(image->Load("data/park02.exr"));
+    auto& data2 = image->GetData();
+    CheckCRC(std::get<0>(data2), std::get<1>(data2), 2283193732);
 }
 
 BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc6h, T, FixturesAll, T)
 {
-	// Disable HW GPU support when running on CI
-	if (T::isNull)
-		return;
+    // Disable HW GPU support when running on CI
+    if (T::isNull)
+        return;
 
-	// skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
+    // skip BC6H/BC7 compression on CI debug because it will make Appveyor timeout
 #ifdef _DEBUG
-	if (IsAppVeyor())
-		return;
+    if (IsAppVeyor())
+        return;
 #endif
 
-	auto image = std::make_unique<ninniku::cmftImage>();
+    auto image = std::make_unique<ninniku::cmftImage>();
 
-	BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
+    BOOST_REQUIRE(image->Load("data/whipple_creek_regional_park_01_2k.hdr"));
 
-	auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
-	auto& dx = ninniku::GetRenderer();
-	auto srcTex = dx->CreateTexture(srcParam);
-	auto res = std::make_unique<ninniku::ddsImage>();
+    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
+    auto& dx = ninniku::GetRenderer();
+    auto srcTex = dx->CreateTexture(srcParam);
+    auto res = std::make_unique<ninniku::ddsImage>();
 
-	BOOST_REQUIRE(res->InitializeFromTextureObject(dx, srcTex.get()));
+    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, srcTex.get()));
 
-	ChangeDirectory(T::platform);
+    ChangeToOutDirectory(T::platform);
 
-	std::string filename = "dds_saveImage_bc6h.dds";
+    std::string filename = "dds_saveImage_bc6h.dds";
 
-	BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC6H_UF16));
-	BOOST_REQUIRE(std::filesystem::exists(filename));
+    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC6H_UF16));
+    BOOST_REQUIRE(std::filesystem::exists(filename));
 
-	// dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
-	switch (dx->GetType()) {
-	case ninniku::ERenderer::RENDERER_DX12:
-	case ninniku::ERenderer::RENDERER_WARP_DX12:
-		CheckFileCRC(filename, 4073542973);
-		break;
+    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
+    switch (dx->GetType()) {
+        case ninniku::ERenderer::RENDERER_DX12:
+        case ninniku::ERenderer::RENDERER_WARP_DX12:
+            CheckFileCRC(filename, 4073542973);
+            break;
 
-	case ninniku::ERenderer::RENDERER_DX11:
-		CheckFileCRC(filename, 842772517);
-		break;
+        case ninniku::ERenderer::RENDERER_DX11:
+            CheckFileCRC(filename, 842772517);
+            break;
 
-	case ninniku::ERenderer::RENDERER_WARP_DX11:
-		CheckFileCRC(filename, 842772517);
-		break;
+        case ninniku::ERenderer::RENDERER_WARP_DX11:
+            CheckFileCRC(filename, 842772517);
+            break;
 
-	default:
-		throw std::exception("Case should not happen");
-		break;
-	}
+        default:
+            throw std::exception("Case should not happen");
+            break;
+    }
 }
 
 BOOST_FIXTURE_TEST_CASE_TEMPLATE(dds_saveImage_bc7, T, FixturesAll, T)
 {
-	// Disable HW GPU support when running on CI
-	if (T::isNull)
-		return;
+    // Disable HW GPU support when running on CI
+    if (T::isNull)
+        return;
 
-	auto& dx = ninniku::GetRenderer();
+    auto& dx = ninniku::GetRenderer();
 
-	auto image = std::make_unique<ninniku::genericImage>();
+    auto image = std::make_unique<ninniku::genericImage>();
 
-	BOOST_REQUIRE(image->Load("data/banner.png"));
+    BOOST_REQUIRE(image->Load("data/banner.png"));
 
-	auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
-	auto srcTex = dx->CreateTexture(srcParam);
-	auto needFix = image->IsRequiringFix();
-	auto resized = ResizeImage(dx, srcTex, needFix, T::shaderRoot);
-	auto res = std::make_unique<ninniku::ddsImage>();
+    auto srcParam = image->CreateTextureParam(ninniku::RV_SRV);
+    auto srcTex = dx->CreateTexture(srcParam);
+    auto needFix = image->IsRequiringFix();
+    auto resized = ResizeImage(dx, srcTex, needFix, T::shaderRoot);
+    auto res = std::make_unique<ninniku::ddsImage>();
 
-	BOOST_REQUIRE(res->InitializeFromTextureObject(dx, resized.get()));
+    BOOST_REQUIRE(res->InitializeFromTextureObject(dx, resized.get()));
 
-	ChangeDirectory(T::platform);
+    ChangeToOutDirectory(T::platform);
 
-	std::string_view filename = "dds_saveImage_bc7.dds";
+    std::string_view filename = "dds_saveImage_bc7.dds";
 
-	BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC7_UNORM));
-	BOOST_REQUIRE(std::filesystem::exists(filename));
+    BOOST_REQUIRE(res->SaveCompressedImage(filename, dx, DXGI_FORMAT_BC7_UNORM));
+    BOOST_REQUIRE(std::filesystem::exists(filename));
 
-	// dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
-	switch (dx->GetType()) {
-	case ninniku::ERenderer::RENDERER_DX12:
-		CheckFileCRC(filename, 2657823934);
-		break;
+    // dx11 can use GPU compression while dx12 uses CPU since DirectXTex doesn't support it
+    switch (dx->GetType()) {
+        case ninniku::ERenderer::RENDERER_DX12:
+            CheckFileCRC(filename, 2657823934);
+            break;
 
-	case ninniku::ERenderer::RENDERER_DX11:
-		CheckFileCRC(filename, 3153192394);
-		break;
+        case ninniku::ERenderer::RENDERER_DX11:
+            CheckFileCRC(filename, 3153192394);
+            break;
 
-	case ninniku::ERenderer::RENDERER_WARP_DX11:
-		CheckFileCRC(filename, 2046772967);
-		break;
+        case ninniku::ERenderer::RENDERER_WARP_DX11:
+            CheckFileCRC(filename, 2046772967);
+            break;
 
-	case ninniku::ERenderer::RENDERER_WARP_DX12:
-		CheckFileCRC(filename, 222998642);
-		break;
+        case ninniku::ERenderer::RENDERER_WARP_DX12:
+            CheckFileCRC(filename, 222998642);
+            break;
 
-	default:
-		throw std::exception("Case should not happen");
-		break;
-	}
+        default:
+            throw std::exception("Case should not happen");
+            break;
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
